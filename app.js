9c7dd0044d84b53edf67b372a81eee485e5d075a
moreyl <yan.alferov@yahoo.com>
2019-05-01 21:20:02 +0400

    lesson3

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ new Vue({
     el: '#app',
     data: {
         title: 'Becoming a Vue ninja',
-        wage: 10
-    
-
+        wage: 10,
+        coords: {
+            x: 0,
+            y: 0
+        } 
     },
     methods: {
-        changeWage(amount) {
-            this.wage += amount
+       
+        logEvent(e){
+            console.log(e)
+        },
+        logCoords(e) {
+            this.coords.x = e.offsetX
+            this.coords.y = e.offsetY
         }
-        
     }
 
 })
